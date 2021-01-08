03a910ce7f4c0abf7d15d19ecf18e306cc1ea09b
zhyq <1064639871@qq.com>
2021-01-08 16:30:05 +0800

    多target创建，目录整理
    提出公共文件

--- opengl1/opengl1/main.cpp
+++ opengl1/opengl1/main.cpp
@@ -1,3 +1,8 @@
+/**
+ learn  https://learnopengl-cn.github.io/01%20Getting%20started/04%20Hello%20Triangle/
+ 
+ */
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
---
+++ opengl1/opengl2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl2
+//
+//  Created by staff on 2021/1/8.
+//  Copyright © 2021 zyq_opengl_exercise. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

71a548f297a91f2223522bcfb68804fa1fffdd07
zhyq <1064639871@qq.com>
2021-01-18 20:03:59 +0800

    坐标系统：主程序Vclip=Mprojection⋅Mview⋅Mmodel⋅Vlocal
    https://learnopengl.com/code_viewer_gh.php?code=src/1.getting_started/6.1.coordinate_systems/coordinate_systems.cpp

---
+++ coordinate/common/Shader.hpp
@@ -0,0 +1,44 @@
+//
+//  Shader.hpp
+//
+//  Created by staff on 2021/1/11.
+//  Copyright © 2021 zyq_opengl_exercise. All rights reserved.
+//  https://learnopengl-cn.github.io/01%20Getting%20started/05%20Shaders/
+//  公用着色器类
+//
+
+#ifndef Shader_hpp
+#define Shader_hpp
+
+#include <stdio.h>
+#include <glad/glad.h> // 包含glad来获取所有的必须OpenGL头文件
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+    // 程序ID
+    unsigned int ID;
+
+    // 构造器读取并构建着色器
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    // 使用/激活程序
+    void use();
+    
+    // uniform工具函数
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+    void setMatrix4fv(const std::string &name, const GLfloat *value) const;
+    
+private:
+    // utility function for checking shader compilation/linking errors.
+    void checkCompileErrors(unsigned int shader, std::string type);
+};
+
+
+#endif /* Shader_hpp */

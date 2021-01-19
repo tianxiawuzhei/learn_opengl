2bb980b0c2fc2ef1ea32820f3b4e2d903d4f9e66
zhyq <1064639871@qq.com>
2021-01-19 11:13:49 +0800

    坐标系统：更多的3D
    https://learnopengl.com/code_viewer_gh.php?code=src/1.getting_started/6.3.coordinate_systems_multiple/coordinate_systems_multiple.cpp

--- coordinate/common/Shader.hpp
+++ coordinate/common/Shader.hpp
@@ -13,6 +13,10 @@
 #include <stdio.h>
 #include <glad/glad.h> // 包含glad来获取所有的必须OpenGL头文件
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -33,6 +37,21 @@ public:
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+    void setVec2(const std::string &name, const glm::vec2 &value) const;
+    void setVec2(const std::string &name, float x, float y) const;
+      // ------------------------------------------------------------------------
+    void setVec3(const std::string &name, const glm::vec3 &value) const;
+    void setVec3(const std::string &name, float x, float y, float z) const;
+      // ------------------------------------------------------------------------
+    void setVec4(const std::string &name, const glm::vec4 &value) const;
+    void setVec4(const std::string &name, float x, float y, float z, float w);
+      // ------------------------------------------------------------------------
+    void setMat2(const std::string &name, const glm::mat2 &mat) const;
+      // ------------------------------------------------------------------------
+    void setMat3(const std::string &name, const glm::mat3 &mat) const;
+      // ------------------------------------------------------------------------
+    void setMat4(const std::string &name, const glm::mat4 &mat) const;
+    
     void setMatrix4fv(const std::string &name, const GLfloat *value) const;
     
 private:

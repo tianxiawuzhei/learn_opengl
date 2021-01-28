396b4865629220a03c044f001dfb944959237b27
zhyq <1064639871@qq.com>
2021-01-28 15:18:39 +0800

    基础光照：在观察空间（而不是世界空间）中计算冯氏光照
    https://learnopengl.com/code_viewer.php?code=lighting/basic_lighting-exercise2

---
+++ lighting/basiclighting/shader_res/shader_lighting_view.fs
@@ -0,0 +1,29 @@
+#version 330 core
+out vec4 FragColor;
+in vec3 Normal;
+in vec3 FragPos;
+in vec3 LightPos;   // extra in variable, since we need the light position in view space we calculate this in the vertex shader
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+
+void main()
+{
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(LightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    
+    float specularStrength = 0.5;
+    // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
+    vec3 viewDir = normalize(-FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 256);
+    vec3 specular = specularStrength * spec * lightColor;
+    
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
